b7f9a501574455e6f162e033ddb57df8c92bea16
Sristi27 <sristi2705@gmail.com>
2020-09-29 20:24:10 +0530

    Updates

---
+++ precache-manifest.23827914757ad3c41708cfeba1fa0e3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92057265f6957a5b25b6eca1da5439a",
+    "url": "/find-your-food/index.html"
+  },
+  {
+    "revision": "f2c91a95f988eac7ff03",
+    "url": "/find-your-food/static/css/main.7af6f40b.chunk.css"
+  },
+  {
+    "revision": "e37321ab6efda19b675f",
+    "url": "/find-your-food/static/js/2.d2131096.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/find-your-food/static/js/2.d2131096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c91a95f988eac7ff03",
+    "url": "/find-your-food/static/js/main.658e60ef.chunk.js"
+  },
+  {
+    "revision": "fa1c40bbb4f14ba42f17",
+    "url": "/find-your-food/static/js/runtime-main.bba8c1fd.js"
+  }
+]);
